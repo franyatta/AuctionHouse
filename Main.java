ac757305c779936c0712bdb772bf88e7c61ba6f0
franyatta <95552724+franyatta@users.noreply.github.com>
2024-03-21 22:33:27 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+/*
+ * Francine Vo
+ * Project 2
+ */
+
+import utility.Utilites;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Create Utilities object and invoke main menu method
+		Utilites menu = new Utilites();
+		menu.viewMainMenu();
+	}
+
+}
